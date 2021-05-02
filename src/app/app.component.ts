9c188cb163af3974669d7e116897ceb275345310
Umar Bunyaminu <umar.bunyaminu@amalitech.org>
2021-05-02 11:04:44 +0000

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {  FormBuilder } from '@angular/forms';
+import { Todo } from './todo';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,62 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'TodoList';
+  constructor(public FormBuilder:FormBuilder){}
+
+  todoList: Todo[] = [];
+  todoForm = this.FormBuilder.group(
+    {
+    title:''
+  }
+  )
+  
+  Add():void{
+    if(this.todoList.filter(todo => todo.title === this.todoForm.value.title).length == 0){
+      this.todoList.push(new Todo(this.todoForm.value.title));
+      this.todoForm.reset();
+      console.warn(this.todoList)
+    }
+    else{
+      alert("Duplicate todo list Title not allowed...");
+      this.todoForm.reset();
+    }
+
+  }
+  
+  getTodoList():void{
+
+  }
+
+  Remove(title:string){
+    for(let i = 0; i < this.todoList.length; i++){
+      if(title === this.todoList[i].title){
+        this.todoList.splice(i,1);
+      }
+    }
+    console.warn(this.todoList)
+  }
+
+  RemoveCompleted():void{
+    for(let i = 0; i<this.todoList.length; i++){
+      if(this.todoList[i].done){
+        this.todoList.splice(i,1);
+      }
+    }
+  }
+
+  RemoveAll():void{
+    this.todoList.splice(0,this.todoList.length);
+  }
+
+  TodoChecked(value:boolean, title:string){
+    for(let i = 0; i < this.todoList.length; i++){
+      if(title === this.todoList[i].title){
+        this.todoList[i].done = value;
+        console.warn(this.todoList[i].done);
+      }
+    }
+    
+  }
+
 }
+
